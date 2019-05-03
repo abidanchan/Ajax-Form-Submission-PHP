8d3f536fd849843a0aca23f1b9a6530205df4520
abidanchan <abidanchan@gmail.com>
2019-05-03 11:39:19 +0500

    Ajax request updated

--- submitdata.php
+++ submitdata.php
@@ -1,3 +1,19 @@
 <?php
-$conn= new mysqli($host,$user,$pass,$db);
+$name=$_POST['name'];
+$db="my_testdb";
+$host="localhost";
+$user="root";
+$pass="";
+$conn=new mysqli($host, $user, $pass, $db); 
+$name=$_POST['name'];
+$sql= "insert into user(name) values('$name')";
+if($conn->query($sql)===TRUE)
+{
+	echo 'success';
+}
+else
+{
+	echo "Error". $sql. "<br>" .$conn->error; 
+	
+}
 ?>
